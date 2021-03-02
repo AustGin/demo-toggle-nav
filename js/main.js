8835936ab204805631b50a4e11712f2d0ebb3b1a
Austen Ginther <aginther3@nait.ca>
2021-03-01 18:43:22 -0700

    followed demo
    Javascript toggle nav lesson

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 // js code to trigger the button goes here
+/* Step 1: query(search) the document and select the element the user will click on to make the nav show and hide */
+const button = document.querySelector('.hamburger-btn');   
+
+/* Step 2: Place a click event on the button */
+button.addEventListener('click', () => {
+    /* when the button is clicked, grab the nav element and add the special class */
+    document.querySelector('nav').classList.toggle('show-nav');
+});
+
+/* note: when place a class name inside the classList method do not include the. It will already expect class name */
